a739e1a03ea037ddca4c926589c656e137b1c6dc
José Lucas <105160291+LucasDev-18@users.noreply.github.com>
2022-10-20 20:24:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,27 +16,19 @@ abas.forEach(aba => {
   })
 }) 
 
-//========== ABAS ==============
 function selecionarAba(aba) {
-  // quando o usuário clicar, desmarcar a aba selecionada
   
   const abaSelecionada = document.querySelector(".aba.selecionado")
   abaSelecionada.classList.remove("selecionado")
-  
-  // marcar a aba clicada como selecionada
   aba.classList.add("selecionado")
   
 }
 
-//============CONTEÚDO DAS ABAS==================
 function mostrarInformacoesDaAba(aba) {
-  
-  // Esconder o conteúdo anterior
 
   const informacaoSelecionada = document.querySelector(".informacao.selecionado")
    informacaoSelecionada.classList.remove("selecionado") 
-
-    // mostrar o conteúdo da aba selecionada
+    
    const informacaoDaAba = `informacao-${aba.id}`
     
     console.log(informacaoDaAba);
